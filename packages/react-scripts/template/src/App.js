8ca8bdb89a7d11f4083f805badf3eff66cc64f90
SovereignMusic <mark@sovereignmusicllp.com>
2017-10-21 22:46:52 -0500

    returning results, still need to deliver them as a complete array

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -6,18 +6,36 @@ import { Dependencies } from './utils/Dependencies';
 
 class App extends Component {
   displayDependecyOutput() {
-    return 'Dependency Injection';
+    const results = new Set(); // ES6, initialize a Set when you hand it an Array.
+    // ES6, initialize Data set with Map
+    const packagesWithDeps = new Map(
+      Dependencies.map(deps => deps.split(': '))
+    );
+
+    const loopOverPackagesWithDeps = (i, key) => {
+      if (packagesWithDeps.get(key)) {
+        loopOverPackagesWithDeps(i, packagesWithDeps.get(key));
+        packagesWithDeps.set(key, null);
+      }
+      results.add(key);
+    };
+
+    packagesWithDeps.forEach(loopOverPackagesWithDeps);
+    for (let result of results) {
+      return `Dependency: ${result}`;
+    }
+    console.log(results);
+    return <div>{[...results]}</div>;
   }
 
   render() {
-    console.log(Dependencies);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Pluralsight Code Challenge</h1>
         </header>
-        <div>{this.displayDependecyOutput()}</div>
+        <div>{this.displayDependecyOutput(Dependencies)}</div>
       </div>
     );
   }
--- packages/react-scripts/template/src/Dependencies.test.js
+++ packages/react-scripts/template/src/Dependencies.test.js
@@ -1,6 +1,11 @@
 import { expect } from 'chai';
 import { Dependencies } from './utils/Dependencies';
+import { results } from './App';
 
 it('should be an array of dependencies', () => {
   expect(Dependencies.length === 6).to.be.true;
 });
+
+// it('should be an object with entries', () => {
+//   return;
+// });
